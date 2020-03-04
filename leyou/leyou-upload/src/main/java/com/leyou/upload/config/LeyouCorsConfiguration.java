c5ec04d92ece4c91950fa66445561325a4130495
Jacob-ZZ <jacobzzq@qq.com>
2020-03-04 21:30:40 +0800

    实现文件上传

---
+++ leyou/leyou-upload/src/main/java/com/leyou/upload/config/LeyouCorsConfiguration.java
@@ -0,0 +1,28 @@
+package com.leyou.upload.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class LeyouCorsConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter(){
+
+        //初始化cors配置对象
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //允许跨域的域名 允许的域,不要写*，否则cookie就无法使用了
+        corsConfiguration.addAllowedOrigin("http://manage.leyou.com");
+        corsConfiguration.setAllowCredentials(true);//允许携带cookie
+        corsConfiguration.addAllowedMethod("*");//代表所有的请求方法
+        corsConfiguration.addAllowedHeader("*");//允许携带所有头信息
+        //初始化cors配置元对象
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
+        //返回corsFilter实例，参数：cors配置元对象
+        return new CorsFilter(corsConfigurationSource);
+    }
+}
